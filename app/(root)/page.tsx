dd8591f3a1c0ca1a8b5cc8bda00cacf6d8c4f61f
Pranjal <pranjalkmaurya@gmail.com>
2023-12-04 19:40:36 +0530

    Clerk setup done

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import { icons } from "lucide-react";
+
+export default function Home() {
+  return <div>
+    <Button size='default' variant='destructive'>Click me</Button>
+  </div>
+}
